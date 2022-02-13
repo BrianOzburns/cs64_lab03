6d06e72fc720ecd133b6e0513d446170b0ce628c
Brian Ozawa Burns <brianozawaburns@csilvm-03.cs.ucsb.edu>
2022-02-13 11:21:23 -0800

    print_reverse

--- print_reverse_array.asm
+++ print_reverse_array.asm
@@ -11,6 +11,26 @@
 printA:
     # TODO: Write your function code here
 
+	move $t0 $a0
+	move $t8 $a0
+
+	li $t1 4
+	mult $a1 $t1
+	mflo $t1
+	addi $t1 $t1 -4 
+	addu $t0 $t0 $t1 # index of last element in the array
+
+	li $v0 1
+
+	loop:
+		lw $a0 0($t0)
+		syscall
+
+		addi $t0 $t0 -4
+		bge $t0 $t8 loop
+
+	jr $ra
+
 main:
 	li $v0, 4
 	la $a0, cout
@@ -23,4 +43,6 @@ main:
 
 exit:
 	# TODO: Write code to properly exit a SPIM simulation
+	li $v0 10
+	syscall
 
